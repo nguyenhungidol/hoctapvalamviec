582c30e91ac9fa0315656388000e75edf7e02909
nguyenhungidol <89452554+nguyenhungidol@users.noreply.github.com>
2021-10-02 15:28:59 +0700

    commit

--- học c++/codelearn.cpp
+++ học c++/codelearn.cpp
@@ -1,34 +1,76 @@
-#include<iostream>
-
+#include <iostream>
 using namespace std;
-
 class ArrayCalculator
 {
 public:
-    static int sumOfArray(int arr[], int n)
+    static int maxOfArray(int arr[], int n)
     {
-        int sum = 0;
+        int maxValue = arr[0];
         for (int i = 0; i < n; i++)
         {
-            sum += arr[i];
+            for (int j = i + 1; j < n; i++)
+            {
+                if (arr[i] > arr[j])
+                {
+                    maxValue = arr[i];
+                }
+            }
         }
-        return sum;
+        return maxValue;
     }
-    static double sumOfArray(double arr[], int n)
+    static double maxOfArray(double arr[], int n)
     {
-        double sum = 0;
+        double maxValue = arr[0];
         for (int i = 0; i < n; i++)
         {
-            sum += arr[i];
+            for (int j = i + 1; j < n; i++)
+            {
+                if (arr[i] > arr[j])
+                {
+                    maxValue = arr[i];
+                }
+            }
         }
-    return sum;
+        return maxValue;
+    }
+    static double minOfArray(int arr[], int n)
+    {
+        double minValue = arr[0];
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; i++)
+            {
+                if (arr[i] < arr[j])
+                {
+                    minValue = arr[i];
+                }
+            }
+        }
+        return minValue;
+    }
+    static double minOfArray(double arr[], int n)
+    {
+        double minValue = arr[0];
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; i++)
+            {
+                if (arr[i] < arr[j])
+                {
+                    minValue = arr[i];
+                }
+            }
+        }
+        return minValue;
     }
 };
 int main()
 {
-    int arr1[] = { 3, 4, 2 };
-	double arr2[] = { 1.3, 4.2, 6.7 };
-	cout << ArrayCalculator::sumOfArray(arr1, sizeof(int)) << endl;
-	cout << ArrayCalculator::sumOfArray(arr2, sizeof(double)) << endl;
+    int arr1[]={3, 7, 9, -5};
+    double arr2[]={3.4, 2.9, -6.8};
+    cout << ArrayCalculator::maxOfArray(arr1, sizeof(arr1) / sizeof(int)) << endl;
+    cout << ArrayCalculator::maxOfArray(arr2, sizeof(arr2) / sizeof(double)) << endl;
+    cout << ArrayCalculator::minOfArray(arr1, sizeof(arr1) / sizeof(int)) << endl;
+    cout << ArrayCalculator::minOfArray(arr2, sizeof(arr2) / sizeof(double)) << endl;
     return 0;
 }
