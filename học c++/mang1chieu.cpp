95f6e80d81481dc6b86fd90ef12c4e502a734ce7
nguyenhungidol <89452554+nguyenhungidol@users.noreply.github.com>
2021-11-01 08:40:13 +0700

    commit

--- học c++/mang1chieu.cpp
+++ học c++/mang1chieu.cpp
@@ -1,58 +1,12 @@
-#include <iostream>
-
+#include<iostream>
 using namespace std;
-
-int kiemtra(int a[], int n)
-{
-	int c25 = 0;
-	int c50 = 0;
-	for (int i = 0; i < n; i++)
-	{
-		if (a[i] == 25)//neu nguoi do co to tien 25
-			c25++;//thi tang so tien to 25
-		else if (a[i] == 50)//con neu khach co to tien to 50
-		{
-			if (c25 == 0)//ban dau neu chua co to 25 thi khong the ban ve
-				return 0;//dung lai 
-			c25--;//neu co thi tra lai to tien 25 va co them to 50
-			c50++;
-		}
-		else//con lai neu khach co to 100
-		{
-			if (c25 == 0 || (c25 * 25 + c50 * 50 < 75))//khong the tra lai duoc nen k the ban ve
-			{
-				return 0;
-			}
-			else//neu co to 25 va 50
-			{
-				if (c50 != 0 && c25 != 0)
-				{
-					c50--;//uu tien cach tra lai bang to 25 va 50
-					c25--;
-				}
-				else
-				{
-					c25 -= 3;//khong co to 50 thi tra lai bang 3 to 25
-				}
-			}
-		}
-	}
-	return 1;//thi duyet cac dieu kien tren thi tra lai dung
-}
-int main()
-{
-	int n;
-	cin >> n;
-	int a[n];
-	for (int i = 0; i < n; i++)
-	{
-		cin >> a[i];
-	}
-	if (kiemtra(a, n))// dieu kien dung
-		cout << "YES";
-	else
-	{
-		cout << " NO";// dieu kien sai
-	}
-	return 0;
+int main(){
+    char x;
+    cin>>x;
+    if(x='z'){
+        cout<<"a";
+    }else if(x>=a && x<z){
+       cout<<x+1;
+    }
+ return 0;
 }
